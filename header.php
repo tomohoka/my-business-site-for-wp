a977f0e6426364ca2d9f4c6765171f4168cb55a2
root <root@wordpress-dev-kusanagi-1-vm.asia-northeast1-b.c.ferrous-quest-291708.internal>
2020-11-05 21:13:34 +0900

    パーツファイルに分割。

---
+++ header.php
@@ -0,0 +1,23 @@
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+<meta name="description" content="" />
+
+<title><?php the_title(); ?></title>
+
+<!-- Favicon-->
+<link rel="icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); ?>/dist/assets/img/favicon.ico" />
+
+<!-- Font Awesome icons (free version)-->
+<script src="//use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+
+<!-- Google fonts-->
+<link href="//fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
+<link href="//fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
+
+<!-- Third party plugin CSS-->
+<link href="//cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
+
+<!-- Core theme CSS (includes Bootstrap)-->
+<link href="<?php echo get_template_directory_uri(); ?>/dist/css/styles.css" rel="stylesheet" />
+
+<?php wp_head(); ?>
--- index.php
+++ index.php
@@ -1,22 +1,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
     <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Creative - Start Bootstrap Theme</title>
-        <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); ?>/dist/assets/img/favicon.ico" />
-        <!-- Font Awesome icons (free version)-->
-        <script src="//use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
-        <!-- Google fonts-->
-        <link href="//fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
-        <link href="//fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
-        <!-- Third party plugin CSS-->
-        <link href="//cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
-        <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="<?php echo get_template_directory_uri(); ?>/dist/css/styles.css" rel="stylesheet" />
+        <?php get_header(); ?>
     </head>
     <body id="page-top">
         <!-- Navigation-->
@@ -194,13 +179,6 @@
         <footer class="bg-light py-5">
             <div class="container"><div class="small text-center text-muted">Copyright © 2020 - Start Bootstrap</div></div>
         </footer>
-        <!-- Bootstrap core JS-->
-        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="//stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
-        <!-- Third party plugin JS-->
-        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
-        <script src="//cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
-        <!-- Core theme JS-->
-        <script src="<?php echo get_template_directory_uri(); ?>/dist/js/scripts.js"></script>
+        <?php get_footer(); ?>
     </body>
 </html>
